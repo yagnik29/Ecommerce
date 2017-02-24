3576c7fc1586cfd930ac74bbc6b244179beee8e6
yagnik29 <yagnikprajapati@gmail.com>
2017-02-24 12:32:52 +0530

    Initial commit+SplashScreen+BasicActivity

---
+++ app/src/main/java/demo/example/com/ecommerce/MainActivity.java
@@ -0,0 +1,28 @@
+package demo.example.com.ecommerce;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(MainActivity.this,Login.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
