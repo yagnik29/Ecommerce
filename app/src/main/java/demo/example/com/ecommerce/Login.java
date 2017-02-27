dfbda75a17db3b9a5235db1aeb46207b0b3a53dc
yagnik29 <yagnikprajapati@gmail.com>
2017-02-27 14:34:00 +0530

    Register_Frag

--- app/src/main/java/demo/example/com/ecommerce/Login.java
+++ app/src/main/java/demo/example/com/ecommerce/Login.java
@@ -2,6 +2,7 @@ package demo.example.com.ecommerce;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -30,8 +31,13 @@ public class Login extends AppCompatActivity {
         new_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent registerscreen = new Intent(Login.this, Registration.class);
-                startActivity(registerscreen);
+                Register registration_frag = new Register();
+                android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.content_login,registration_frag);
+                ft.addToBackStack("A");
+                ft.commit();
+
+
             }
         });
 
---
+++ app/src/main/java/demo/example/com/ecommerce/Register.java
@@ -0,0 +1,30 @@
+package demo.example.com.ecommerce;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Register extends android.app.Fragment {
+
+
+    public Register() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_register, container, false);
+        return v;
+    }
+
+}
