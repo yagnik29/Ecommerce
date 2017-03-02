43b77f2e0666cd0bc736b0f82a453d7f07ab82e7
yagnik29 <yagnikprajapati@gmail.com>
2017-03-02 13:42:35 +0530

    Register_Frag

--- app/src/main/java/demo/example/com/ecommerce/Register.java
+++ app/src/main/java/demo/example/com/ecommerce/Register.java
@@ -1,11 +1,14 @@
 package demo.example.com.ecommerce;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -13,6 +16,7 @@ import android.view.ViewGroup;
  */
 public class Register extends android.app.Fragment {
 
+    Button register;
 
     public Register() {
         // Required empty public constructor
@@ -24,6 +28,13 @@ public class Register extends android.app.Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.fragment_register, container, false);
+        register = (Button) v.findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Registered", Toast.LENGTH_SHORT).show();
+            }
+        });
         return v;
     }
 
