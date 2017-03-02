c1cd9d06632613fe5f3dc574704aed7f6ab1c3e5
yagnik29 <yagnikprajapati@gmail.com>
2017-03-02 16:15:20 +0530

    Description + design changes

--- app/src/main/java/demo/example/com/ecommerce/HomeScreen.java
+++ app/src/main/java/demo/example/com/ecommerce/HomeScreen.java
@@ -1,13 +1,17 @@
 package demo.example.com.ecommerce;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class HomeScreen extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,20 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_homescreen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Description description_frag = new Description();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.content_homescreen,description_frag);
+                ft.addToBackStack("B");
+                ft.commit();
+
+            }
+        });
+
 
     }
 
--- app/src/main/java/demo/example/com/ecommerce/Login.java
+++ app/src/main/java/demo/example/com/ecommerce/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,10 +15,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        */
-        login = (Button) findViewById(R.id.login);
+
+        login = (Button) findViewById(R.id.btnLogin);
         new_register = (Button) findViewById(R.id.newregister);
 
         login.setOnClickListener(new View.OnClickListener() {
@@ -36,8 +37,6 @@ public class Login extends AppCompatActivity {
                 ft.replace(R.id.content_login,registration_frag);
                 ft.addToBackStack("A");
                 ft.commit();
-
-
             }
         });
 
