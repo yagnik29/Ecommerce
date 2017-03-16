de4ca40daa9c2c82d3a599903a639197fdf0afb9
yagnik <yagnikprajapati@gmail.com>
2017-03-16 14:53:15 +0530

    Minor change

--- app/src/main/java/demo/example/com/ecommerce/Login.java
+++ app/src/main/java/demo/example/com/ecommerce/Login.java
@@ -2,17 +2,18 @@ package demo.example.com.ecommerce;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
-    Button login,new_register;
+    Button login;
     EditText username , password;
+    TextView new_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +22,7 @@ public class Login extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         login = (Button) findViewById(R.id.btnLogin);
-        new_register = (Button) findViewById(R.id.newregister);
+        new_register = (TextView) findViewById(R.id.newregister);
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
 
