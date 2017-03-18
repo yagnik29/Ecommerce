242f19778658832e25160175be172a63a8356c3f
Yash <yagnikprajapati@gmail.com>
2017-03-18 16:02:28 +0530

    Added View Flipper

--- app/src/main/java/demo/example/com/ecommerce/HomeScreen.java
+++ app/src/main/java/demo/example/com/ecommerce/HomeScreen.java
@@ -10,14 +10,20 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class HomeScreen extends AppCompatActivity {
 
     ImageView imageView;
     TabLayout tabLayout;
     ViewPager viewPager;
+    ViewFlipper viewFlipper;
+
+    int[] images = {R.drawable.litchi, R.drawable.mango, R.drawable.pineapple};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,22 @@ public class HomeScreen extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
         tabLayout = (TabLayout) findViewById(R.id.tablayout);
         viewPager = (ViewPager) findViewById(R.id.pager);
+        viewFlipper = (ViewFlipper) findViewById(R.id.flipper);
+
+
+        for (int i = 0; i < images.length; i++) {
+            imageView = new ImageView(this);
+            imageView.setImageResource(images[i]);
+            viewFlipper.addView(imageView);
+        }
+
+        Animation in = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        Animation out = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+
+        viewFlipper.setInAnimation(in);
+        viewFlipper.setOutAnimation(out);
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
 
 
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +58,7 @@ public class HomeScreen extends AppCompatActivity {
             public void onClick(View view) {
                 Description description_frag = new Description();
                 FragmentTransaction ft = getFragmentManager().beginTransaction();
-                ft.replace(R.id.content_homescreen,description_frag);
+                ft.replace(R.id.content_homescreen, description_frag);
                 ft.addToBackStack("B");
                 ft.commit();
 
@@ -44,10 +66,10 @@ public class HomeScreen extends AppCompatActivity {
         });
 
         Tab_Adapter tab_adapter = new Tab_Adapter(getSupportFragmentManager());
-        tab_adapter.addFragment(new Electroincs(),"Electronic");
-        tab_adapter.addFragment(new Appliances(),"Appliances");
-        tab_adapter.addFragment(new Clothing(),"Cloths");
-        tab_adapter.addFragment(new Books(),"Books");
+        tab_adapter.addFragment(new Electroincs(), "Electronic");
+        tab_adapter.addFragment(new Appliances(), "Appliances");
+        tab_adapter.addFragment(new Clothing(), "Cloths");
+        tab_adapter.addFragment(new Books(), "Books");
 
         viewPager.setAdapter(tab_adapter);
         tabLayout.setupWithViewPager(viewPager);
