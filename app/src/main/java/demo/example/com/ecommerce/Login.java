6bdfeed68f39a6e77d183e6d068cb38c1f16c514
yagnik29 <yagnikprajapati@gmail.com>
2017-04-07 17:41:22 +0530

    Addition ->Jason ->Login comparison

--- app/src/main/java/demo/example/com/ecommerce/Login.java
+++ app/src/main/java/demo/example/com/ecommerce/Login.java
@@ -1,26 +1,54 @@
 package demo.example.com.ecommerce;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class Login extends AppCompatActivity {
 
     Button login;
     EditText username , password;
     TextView new_register;
+    String url;
+    String EMAIL_ID = "email_id";
+    String PASSWORD = "password";
+    String sUsername;
+    String sPassword;
+    String email,pass;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy =
+                    new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
         login = (Button) findViewById(R.id.btnLogin);
         new_register = (TextView) findViewById(R.id.newregister);
         username = (EditText) findViewById(R.id.username);
@@ -30,12 +58,14 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent homescreen = new Intent(Login.this,Navigational.class);
-                String uname = username.getText().toString();
-                String passw = password.getText().toString();
-                homescreen.putExtra("uname",uname);
-                homescreen.putExtra("passw",passw);
-                startActivity(homescreen);
+                sUsername = username.getText().toString();
+                sPassword = password.getText().toString();
+                try {
+                    url = "http://yagnik.890m.com/webservices/jsonphp.php?" + "email_id=" + sUsername + "&password=" + sPassword;
+                    new GetData().execute();
+                } catch (Exception e) {
+
+                }
             }
         });
 
@@ -51,5 +81,56 @@ public class Login extends AppCompatActivity {
         });
 
     }
+    public class GetData extends AsyncTask< Void, Void, Void> {
 
+        ProgressDialog progressDialog;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(Login.this);
+            progressDialog.setTitle("Loading Data");
+            progressDialog.setMessage("Please Wait");
+        }
+
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            HttpServiceHandler servicedata = new HttpServiceHandler();
+            String result = servicedata.getHttpdata(url);
+            Log.e("Result" , result);
+
+            try {
+                JSONArray jsonArray = new JSONArray(result);
+
+                for (int i=0; i < jsonArray.length(); i++){
+                    JSONObject object = jsonArray.getJSONObject(i);
+                     email = object.getString(EMAIL_ID);
+                    pass= object.getString(PASSWORD);
+
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            try {
+                if(email.equals(sUsername) || pass.equals(sPassword)){
+                    progressDialog.dismiss();
+                    Intent homescreen = new Intent(Login.this,Navigational.class);
+                    startActivity(homescreen);
+                }
+            }catch (Exception e){
+                Toast.makeText(Login.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
 }
--- app/src/main/java/demo/example/com/ecommerce/Register.java
+++ app/src/main/java/demo/example/com/ecommerce/Register.java
@@ -1,22 +1,14 @@
 package demo.example.com.ecommerce;
 
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.okhttp.OkHttpClient;
@@ -30,7 +22,7 @@ import com.squareup.okhttp.Response;
 public class Register extends android.app.Fragment {
 
     Button register;
-    EditText name, email, phone, address, city, password;
+    EditText name, email, phone, address, city, password,pincode;
 
 
     public Register() {
@@ -49,7 +41,6 @@ public class Register extends android.app.Fragment {
         }
 
 
-
         register = (Button) v.findViewById(R.id.register);
         name = (EditText) v.findViewById(R.id.name);
         email = (EditText) v.findViewById(R.id.email);
@@ -57,6 +48,7 @@ public class Register extends android.app.Fragment {
         address = (EditText) v.findViewById(R.id.address);
         city = (EditText) v.findViewById(R.id.city);
         password = (EditText) v.findViewById(R.id.password);
+        pincode = (EditText) v.findViewById(R.id.pincode);
 
 
         register.setOnClickListener(new View.OnClickListener() {
@@ -64,26 +56,52 @@ public class Register extends android.app.Fragment {
             public void onClick(View view) {
 
                 String sName = name.getText().toString();
-                String sEmail= email.getText().toString();
-                String sPhone= phone.getText().toString();
+                String sEmail = email.getText().toString();
+                String sPhone = phone.getText().toString();
                 String sAddress = address.getText().toString();
                 String sCity = city.getText().toString();
+                String sPincode = pincode.getText().toString();
                 String sPassword = password.getText().toString();
 
-                try {
-                    String url = "http://yagnik.890m.com/webservices/registration.php?uname=" + sName +
-                            "&email_id=" + sEmail + "&phone=" + sPhone + "&address1=" + sAddress + "&city=" + sCity + "&password=" + sPassword;
-
-                    OkHttpClient client = new OkHttpClient();
-
-                    Request request = new Request.Builder().url(url).build();
-                    Response response = client.newCall(request).execute();
-
-                    Toast.makeText(getActivity(), response.body().toString(), Toast.LENGTH_SHORT).show();
-                    Toast.makeText(getActivity(), "welcome", Toast.LENGTH_LONG).show();
-                } catch (Exception e) {
-
+                if (sName.length() == 0) {
+                    name.setError("Please Enter Name");
                 }
+                if (sEmail.length() == 0) {
+                    email.setError("Please Enter valid E-mail");
+                }
+                if (sPhone.length() < 10) {
+                    phone.setError("Please Enter valid Phone Number");
+                }
+                if (sAddress.length() == 0) {
+                    address.setError("Please Enter your address");
+                }
+                if (sCity.length() == 0) {
+                    city.setError("Please Enter your curent city name");
+                }
+
+                if (sPincode.length() <6){
+                    pincode.setError("Please enter valid PIN code");
+                }
+
+                if (sPassword.length() <= 8) {
+                    password.setError("Password must be 8 characters long");
+                    password.requestFocus();
+                } else {
+                    try {
+                        String url = "http://yagnik.890m.com/webservices/registration.php?uname=" + sName +
+                                "&email_id=" + sEmail + "&phone=" + sPhone + "&address1=" + sAddress + "&city=" + sCity + "&pincode=" + sPincode + "&password="  + sPassword;
+
+                        OkHttpClient client = new OkHttpClient();
+
+                        Request request = new Request.Builder().url(url).build();
+                        Response response = client.newCall(request).execute();
+
+                        Toast.makeText(getActivity(), response.body().toString(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), "Success", Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                    }
+                }
+
             }
         });
         return v;
